51582da421cb1ac53cbafb0796dc78cebb56a16d
unknown <subhradeepray2017@gmail.com>
2023-02-20 23:50:26 +0530

    User Input

---
+++ user-input.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+   
+echo "Enter the user name => "  
+read first_name  
+echo "The Current User Name is $first_name"  
+echo  
+echo "Enter other users'names => "  
+read name1 name2 name3  
+echo "$name1, $name2, $name3 are the other users."  
