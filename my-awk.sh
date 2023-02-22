8fc96156e83a9825e7df5291e85bbcacad8326d2
unknown <subhradeepray2017@gmail.com>
2023-02-22 20:08:33 +0530

    To Print the Squares of First Numbers From 1 to N

--- my-awk.sh
+++ my-awk.sh
@@ -7,3 +7,6 @@ awk '{print $1,$4}' student.txt
 
 #Printing Lines With More Than 15 Characters
 awk 'length($0) > 15' student.txt
+
+#To Print the Squares of First Numbers From 1 to N
+awk 'BEGIN { for(i=1;i<=10;i++) print "Square of", i, "is => ",i*i; }'
