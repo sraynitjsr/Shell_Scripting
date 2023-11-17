dbac800e4c097ffce77e518f2f1a8267bacb6299
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-11-17 22:57:59 +0530

    Filter all lines based on a condition

--- my-awk.sh
+++ my-awk.sh
@@ -1,3 +1,5 @@
 ### 1. Print specific columns from a file => awk '{print $1, $3}' data.txt
 
 ### 2. Calculate and print the average of a column => awk '{sum += $1} END {print "Average:", sum/NR}' grades.txt
+
+### 3. Filter lines based on a condition => awk '$2 > 70 {print $1, $2}' scores.txt
