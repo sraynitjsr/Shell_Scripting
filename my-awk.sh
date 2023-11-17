44121a9a5c354675815488e54f96c2e9ab3d977a
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-11-17 22:57:10 +0530

    Calculate and print the average of a column

--- my-awk.sh
+++ my-awk.sh
@@ -1,1 +1,3 @@
 ### 1. Print specific columns from a file => awk '{print $1, $3}' data.txt
+
+### 2. Calculate and print the average of a column => awk '{sum += $1} END {print "Average:", sum/NR}' grades.txt
