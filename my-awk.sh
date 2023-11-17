f744312cc865a989cb4195897652e5f8851f3c73
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-11-17 22:59:15 +0530

    Print all the lines that match a specific patter

--- my-awk.sh
+++ my-awk.sh
@@ -3,3 +3,5 @@
 ### 2. Calculate and print the average of a column => awk '{sum += $1} END {print "Average:", sum/NR}' grades.txt
 
 ### 3. Filter lines based on a condition => awk '$2 > 70 {print $1, $2}' scores.txt
+
+#### 4. Print lines that match a specific pattern => awk '/o/ {print}' names.txt
