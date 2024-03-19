d1fc4e2d924563daece67a1886ba5f5299b7caaa
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-03-19 20:28:54 +0530

    Frequency Count of Array Elements Using Shell Script

--- frequency_count.sh
+++ frequency_count.sh
@@ -4,9 +4,10 @@ array=(1 2 3 4 1 2 3 1 2 1)
 declare -A freq
 
 for elem in "${array[@]}"; do
-    ((freq[$elem]++))
+    hash=$(echo "$elem" | md5sum | cut -d ' ' -f1)
+    ((freq[$hash]++))
 done
 
 for key in "${!freq[@]}"; do
-    echo "Element $key: ${freq[$key]} occurrences"
+    echo "Element $(echo "$key" | cut -c1-8): ${freq[$key]} occurrences"
 done
